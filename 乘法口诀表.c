e7c27d507e82aca32cafcefd3165e041923a57ab
yunyuxiangtong <yunyuxiangtong@qq.com>
2026-03-26 22:31:12 +0800

    Add files via upload

---
+++ 乘法口诀表.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	for(int i=1;i<10;i++)
+	{
+		printf("\n");
+		for(int j=1;j<=i;j++)
+		{
+			printf("%d*%d=%d   ",i,j,i*j);
+			
+		}
+		
+	}
+	
+	
+	return 0;
+}
